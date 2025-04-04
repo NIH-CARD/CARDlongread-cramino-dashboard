2fa376d27d83cfa138c1ae59070c86f45fb79a9a
Abraham Moller <mollerabg@biowulf.nih.gov>
2025-04-04 12:50:16 -0400

    New dashboard with grouping and custom colors in legend

--- CARDlongread_cramino_dashboard.py
+++ CARDlongread_cramino_dashboard.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import statistics
 import argparse
 import dataclasses
@@ -60,7 +61,7 @@ def make_summary_statistics_data_frame(input_data_frame, property_names):
     return summary_statistics_df
     
 # make violinplot/swarmplot figure worksheet in output workbook
-def make_violinswarmplot_worksheet(data,input_variable,group_variable,workbook,worksheet_name,x_axis_title=None,cutoff=None,title=None):
+def make_violinswarmplot_worksheet(data,input_variable,group_variable,legend_patches,user_palette,strip_plot_set,workbook,worksheet_name,x_axis_title=None,cutoff=None,title=None):
     # create worksheet for figure output
     worksheet=workbook.create_sheet(worksheet_name)
     # initialize raw data buffer for image
@@ -71,7 +72,10 @@ def make_violinswarmplot_worksheet(data,input_variable,group_variable,workbook,w
     if (group_variable is None):
         # make swarm plot to show how data points overlap with distribution
         # replace color='black'
-        ax = sb.swarmplot(data=data,x=input_variable,color='black')
+        if strip_plot_set is False:
+            ax = sb.swarmplot(data=data,x=input_variable,color='black')
+        elif strip_plot_set is True:
+            ax = sb.stripplot(data=data,x=input_variable,color='black')
         # add violin plot using seaborn (sb.violinplot)
         # increase transparency to improve swarmplot visibility
         # use boxplot since only one "group" shown
@@ -80,8 +84,19 @@ def make_violinswarmplot_worksheet(data,input_variable,group_variable,workbook,w
         # make swarm plot to show how data points overlap with distribution
         # input variable on y axis and group on x axis
         # thus vertical swarm/violinplots instead of horizontal ones when no group specified
-        # replace color='black'
-        ax = sb.swarmplot(data=data,x=group_variable,y=input_variable,color='black')
+        # replace color='black' with hue set to group variable
+        if strip_plot_set is False:
+            # allow user set palette
+            if user_palette is None:
+                ax = sb.swarmplot(data=data,x=group_variable,y=input_variable,hue=group_variable,legend=False)
+            else:
+                ax = sb.swarmplot(data=data,x=group_variable,y=input_variable,hue=group_variable,palette=user_palette,legend=False)
+        elif strip_plot_set is True:
+            # allow user set palette
+            if user_palette is None:
+                ax = sb.stripplot(data=data,x=group_variable,y=input_variable,hue=group_variable,legend=False)
+            else:
+                ax = sb.stripplot(data=data,x=group_variable,y=input_variable,hue=group_variable,palette=user_palette,legend=False)
         # add violin plot using seaborn (sb.violinplot)
         # increase transparency to improve swarmplot visibility
         # include quartile lines in this context (for easily, visually comparing between groups)
@@ -101,10 +116,14 @@ def make_violinswarmplot_worksheet(data,input_variable,group_variable,workbook,w
         # horizontal line if grouped (group variable on x-axis)
         else:
             ax.axhline(y=cutoff,color='red')
+    # add legend with colors if requested
+    if legend_patches is not None:
+        plt.legend(handles=legend_patches)
     # put figure in variable to prep for saving into buffer
     # fig = swarmplot.get_figure()
     # save figure as 200 dpi PNG into buffer
-    fig.savefig(imgdata, format='png', dpi=200)
+    # tight layout to prevent titles from being cut off
+    fig.savefig(imgdata, format='png', dpi=200, bbox_inches='tight')
     # close figure
     fig.clf()
     # make openpyxl image from raw data
@@ -129,6 +148,16 @@ parser.add_argument('-plot_title', action="store", default=None, dest="plot_titl
 parser.add_argument('--plot_cutoff', action=argparse.BooleanOptionalAction, default=True, dest="plot_cutoff", help="Include cutoff lines in violin plots (optional; default true; --no-plot_cutoff to override)")
 # include failed run cutoff to exclude as well
 parser.add_argument('-run_cutoff', action="store", default=1, type=float, dest="run_cutoff", help="Minimum data output per flow cell run to include (optional, 1 Gb default)")
+# add option for stripplot instead of swarmplot (in case of excessive data points)
+parser.add_argument('--strip_plot', action=argparse.BooleanOptionalAction, default=False, dest="strip_plot", help="Show strip plots instead of swarm plots inside violin plots (optional; default false)")
+# add option for color palette
+parser.add_argument('-colors', action="store", default=None, dest="colors", nargs="*", help="Color palette corresponding to sequential groups displayed (e.g., 'blue', 'red', 'blue'); optional and used only if more than one tsv provided.")
+# add option for custom legend colors
+parser.add_argument('-legend_colors', action="store", default=None, dest="legend_colors", nargs="*", help="Colors shown in the legend (e.g., 'blue', 'red'); optional and used only if more color palette included above. Must be palette subset.")
+# add option for custom legend labels
+parser.add_argument('-legend_labels', action="store", default=None, dest="legend_labels", nargs="*", help="Labels for each color in legend in order specified in -legend_colors.")
+# add option to show sample size for groups in grouped violinplots
+parser.add_argument('--group_count', action=argparse.BooleanOptionalAction, default=False, dest="show_group_count", help="Show group count in x-axis labels (optional; default false)")
 
 # parse arguments
 results = parser.parse_args()
@@ -141,7 +170,29 @@ if results.input_file is None:
 if len(results.input_file)>1:
     if len(results.names)<=1:
         quit('ERROR: Multiple input files provided but not multiple names (-names).')
+    elif len(results.names) != len(results.input_file):
+        quit('ERROR: Number of names is different from number of input files.')
+    # test if number of colors different from input files and names
+    elif (results.colors is not None) and (len(results.colors) != len(results.names)):
+        quit("ERROR: Color palette provided, but number of colors doesnt match number of group names.")
         
+# set legend_patches to None by default
+legend_patches=None
+# test if legend colors and labels have proper length
+if (results.legend_colors is not None) or (results.legend_labels is not None):
+    if len(results.legend_colors) != len(results.legend_labels):
+        quit('ERROR: Number of legend colors does not match number of legend labels.')
+    else:
+        # prepare legend patches
+        # make list as long as legend colors (at this point same as legend_labels)
+        legend_patches = [0] * len(results.legend_colors)
+        for idx, i in enumerate(results.legend_colors):
+            legend_patches[idx] = mpatches.Patch(color=i, label=results.legend_labels[idx])
+        
+# test if legend colors provided but not labels or vice versa
+if ((results.legend_colors is not None) and (results.legend_labels is None)) or ((results.legend_colors is None) and (results.legend_labels is not None)):
+    quit('ERROR: Either legend colors or legend labels provided but not both.')
+
 # set default output filename
 if results.output_file is None:
     results.output_file='output_summary_statistics.xlsx'
@@ -150,6 +201,8 @@ if results.output_file is None:
 # case if just one input file provided
 if len(results.input_file)==1:
     cramino_extract_initial=pd.read_csv(results.input_file[0],sep='\t')
+    # first filter out low output runs
+    cramino_extract = cramino_extract_initial[cramino_extract_initial['Yield (Gb)'] > results.run_cutoff]
     grouped=False
 # what if multiple input files provided
 elif len(results.input_file)>1:
@@ -157,16 +210,24 @@ elif len(results.input_file)>1:
     cramino_extract_initial_list=[0] * len(results.input_file)
     for idx, i in enumerate(results.input_file): 
         cramino_extract_initial_list[idx]=pd.read_csv(i,sep='\t')
+        # first filter out low output runs
+        cramino_extract_initial_list[idx]=cramino_extract_initial_list[idx][cramino_extract_initial_list[idx]['Yield (Gb)'] > results.run_cutoff]
         # add group name to each table in list
-        cramino_extract_initial_list[idx]['Group']=results.names[idx]
+        if results.show_group_count is True:
+            # if group count specified, add group count to group name
+            group_count = len(cramino_extract_initial_list[idx])
+            # in this way, show n=717 or similar below group names in all plots
+            cramino_extract_initial_list[idx]['Group']=results.names[idx]
+            cramino_extract_initial_list[idx]['Group and count']=results.names[idx] + "\nn=" + str(group_count)
+            # group_names_list[idx]=results.names[idx] + "\nn=" + str(group_count)
+        else:
+            cramino_extract_initial_list[idx]['Group']=results.names[idx]
     # combine groups into single concatenated data table
-    cramino_extract_initial=pd.concat(cramino_extract_initial_list[:],ignore_index=True)
+    cramino_extract=pd.concat(cramino_extract_initial_list[:],ignore_index=True)
     # set group variable
     grouped=True
 
 # use functions above
-# first filter out low output runs
-cramino_extract = cramino_extract_initial[cramino_extract_initial['Yield (Gb)'] > results.run_cutoff]
 # calculate median mapping q score
 # not needed, add calculation to report parser
 # cramino_extract['Median mapping Q score'] = -10*np.log10((100-cramino_extract['Median identity'])/100)
@@ -195,7 +256,7 @@ elif grouped is True:
     # output data frames and figures to excel spreadsheet
     # intialize writer BEFORE for loop through results names
     writer = pd.ExcelWriter(results.output_file)
-    # loop through all results names
+    # loop through all group names from input
     for idx, i in enumerate(results.names):
         # make data frame
         cramino_summary_statistics_df = make_summary_statistics_data_frame(cramino_extract[cramino_extract['Group'] == i], cramino_summary_statistics_property_names)
@@ -209,6 +270,7 @@ elif grouped is True:
         cramino_summary_statistics_df.to_excel(writer, startrow=start_row, index=False, sheet_name=i + ' statistics')
     # close writer and save workbook AFTER for loop complete
     writer.close()
+# prepare legend patches
 # then add svg figures
 # use openpyxl and pipe image data into new worksheets
 # append new worksheets to existing workbook
@@ -224,9 +286,12 @@ if results.plot_cutoff is True:
     for idx, i in enumerate(cramino_summary_statistics_property_names):
         # include group variable if necessary
         if grouped is False:
-            make_violinswarmplot_worksheet(cramino_extract,i,None,workbook,cramino_plot_worksheet_names[idx],None,cramino_plot_cutoff_array[idx],results.plot_title)
+            make_violinswarmplot_worksheet(cramino_extract,i,None,legend_patches,results.colors,results.strip_plot,workbook,cramino_plot_worksheet_names[idx],None,cramino_plot_cutoff_array[idx],results.plot_title)
         elif grouped is True:
-            make_violinswarmplot_worksheet(cramino_extract,i,cramino_extract['Group'],workbook,cramino_plot_worksheet_names[idx],None,cramino_plot_cutoff_array[idx],results.plot_title)
+            if results.show_group_count is True:
+                make_violinswarmplot_worksheet(cramino_extract,i,cramino_extract['Group and count'],legend_patches,results.colors,results.strip_plot,workbook,cramino_plot_worksheet_names[idx],None,cramino_plot_cutoff_array[idx],results.plot_title)
+            else:
+                make_violinswarmplot_worksheet(cramino_extract,i,cramino_extract['Group'],legend_patches,results.colors,results.strip_plot,workbook,cramino_plot_worksheet_names[idx],None,cramino_plot_cutoff_array[idx],results.plot_title)
 else:
     # sequential violin/swarm plots for each property
     # simplify by iterating through for loop
@@ -235,9 +300,12 @@ else:
     for idx, i in enumerate(cramino_summary_statistics_property_names):
         # include group variable if necessary
         if grouped is False:
-            make_violinswarmplot_worksheet(cramino_extract,i,None,workbook,cramino_plot_worksheet_names[idx],None,None,results.plot_title)
+            make_violinswarmplot_worksheet(cramino_extract,i,None,legend_patches,results.colors,results.strip_plot,workbook,cramino_plot_worksheet_names[idx],None,None,results.plot_title)
         elif grouped is True:
-            make_violinswarmplot_worksheet(cramino_extract,i,cramino_extract['Group'],workbook,cramino_plot_worksheet_names[idx],None,None,results.plot_title)
+            if results.show_group_count is True:
+                make_violinswarmplot_worksheet(cramino_extract,i,cramino_extract['Group and count'],legend_patches,results.colors,results.strip_plot,workbook,cramino_plot_worksheet_names[idx],None,None,results.plot_title)
+            else:
+                make_violinswarmplot_worksheet(cramino_extract,i,cramino_extract['Group'],legend_patches,results.colors,results.strip_plot,workbook,cramino_plot_worksheet_names[idx],None,None,results.plot_title)
 # save workbook when done
 workbook.save(results.output_file)
 
